47510e562fc0d54461dfe9a9953d2132b6c2249b
ShinyGovindan <65227083+ShinyGovindan@users.noreply.github.com>
2020-05-19 16:38:15 +0530

    Add files via upload

---
+++ Models/ProductBusinessRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestDrivenAPI.Models
+{
+    public class ProductBusinessRule
+    {
+        public int RuleId { get; set; }
+        public int ProductTypeId { get; set; }
+        public string BusinessRuleName { get; set; }
+        public bool isActive { get; set; }
+        
+    }
+}
